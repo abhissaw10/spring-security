91035c0eca99b6e8f4c35ab4b737d06da8c7c5e8
Abhishek Sawhney <sawhneyab@sawhneyab-a02.vmware.com>
2021-09-12 08:27:08 +0530

    A&A using database

---
+++ src/main/java/com/example/securitydemo/ApplicationUserDao.java
@@ -0,0 +1,5 @@
+package com.example.securitydemo;
+
+public interface ApplicationUserDao {
+    public ApplicationUser loadUserByUsername(String s);
+}
--- src/main/java/com/example/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securitydemo/security/ApplicationSecurityConfig.java
@@ -1,11 +1,11 @@
 package com.example.securitydemo.security;
 
-import com.example.securitydemo.ApplicationPermission;
-import com.example.securitydemo.ApplicationRole;
+import com.example.securitydemo.ApplicationUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,7 +18,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.Arrays;
 
-import static com.example.securitydemo.ApplicationPermission.*;
+import static com.example.securitydemo.ApplicationPermission.STUDENT_READ;
 import static com.example.securitydemo.ApplicationRole.*;
 
 @Configuration
@@ -28,6 +28,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     PasswordEncoder encoder;
+
+    @Autowired
+    ApplicationUserDetailsService applicationUserDetailsService;
 
 
     @Override
@@ -42,10 +45,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.antMatchers(HttpMethod.GET,"/management/**").hasAnyRole(ADMIN.name(), NEW_ADMIN.name())
                 .anyRequest()
                 .authenticated()
-                .and().httpBasic();
+                .and().formLogin();
     }
 
-    @Override
+    /*@Override
     @Bean
     protected UserDetailsService userDetailsService() {
        return new InMemoryUserDetailsManager(
@@ -53,6 +56,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                        User.builder().username("harish").password(encoder.encode("password")).roles(STUDENT.name()).authorities(STUDENT.getAuthorities()).build(),
                        User.builder().username("vishal").password(encoder.encode("password123")).roles(NEW_ADMIN.name()).authorities(NEW_ADMIN.getAuthorities()).build(),
                        User.builder().username("kartik").password(encoder.encode("password12")).roles(STUDENT.name(), ADMIN.name()).authorities(ADMIN.getAuthorities()).build()));
+    }*/
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder);
+        provider.setUserDetailsService(applicationUserDetailsService);
+        return provider;
+    }
+
 
 }
